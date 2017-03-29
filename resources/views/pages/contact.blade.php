4c8c1494ecdf8250b315de2eceee087ca60a2c57
Bernard Chisumo <bchisumo74@yahoo.com>
2017-03-29 21:09:15 +0100

    Added two pages and link all pages

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,86 +1,102 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Laravel Blog - Contact</title>
 
-    <title>Laravel</title>
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @if (Auth::check())
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ url('/login') }}">Login</a>
-                <a href="{{ url('/register') }}">Register</a>
-            @endif
+<!-- Default  Nav Bar--->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel Blog</a>
         </div>
-    @endif
 
-    <div class="content">
-        <div class="title">Contact  Me</div>
-        <p>I would love to chat</p>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li ><a href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a></li>
+                <li ><a href="{{ route('about') }}">About</a></li>
+                <li class="active"><a href="{{ route('contact') }}">Contact</a></li>
+
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Contact  Me</h1>
+            <hr>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Enter Your Email">
+                </div>
+                <div class="form-group">
+                    <label for="">Subject:</label>
+                    <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter Your Subject">
+                </div>
+                <div class="form-group">
+                    <label for="email">Message:</label>
+                    <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+
+                <input type="submit" value="Send Message" class="btn btn-success">
+
+            </form>
+        </div>
     </div>
-</div>
+</div><!-- End of container --->
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -64,13 +64,39 @@
      <div class="row">
          <div class="col-md-12">
              <div class="jumbotron">
-                 <h1>Hello, world!</h1>
+                 <h1>Welcome to My Blog</h1>
                  <p class="lead">Thank you so much for visiting . This is Laravel Blog .Please read the Latest blog</p>
-                 <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                 <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
              </div>
          </div>
+     </div><!-- End of header .row--->
+
+     <div class="row">
+         <div class="col-md-8" >
+           <div class="post">
+               <h3>Post Title</h3>
+               <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
+               <a href="#" class="btn btn-primary">Read More</a>
+           </div>
+             <hr>
+             <div class="post">
+                 <h3>Post Title</h3>
+                 <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
+                 <a href="#" class="btn btn-primary">Read More</a>
+             </div>
+             <hr>
+             <div class="post">
+                 <h3>Post Title</h3>
+                 <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
+                 <a href="#" class="btn btn-primary">Read More</a>
+             </div>
+             <hr>
+         </div>
+         <div class="col-md-3 col-md-offset-1">
+            <h1>SideBar</h1>
+         </div>
      </div>
- </div>
+ </div><!-- End of container --->
 
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
