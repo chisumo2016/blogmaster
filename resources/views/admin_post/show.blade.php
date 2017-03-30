d393276d1e21eb2c422d5701766d40512e7f187a
Bernard Chisumo <bchisumo74@yahoo.com>
2017-03-30 18:05:36 +0100

    Working with dates from Datbase using Carbon

--- resources/views/admin_post/show.blade.php
+++ resources/views/admin_post/show.blade.php
@@ -1,3 +1,4 @@
+
 @extends('app')
 
 @section('title', '|View Post')
@@ -7,6 +8,29 @@
        <div class="col-md-8">
            <h1>{{ $post->title }}</h1>
            <p class="lead">{{ $post->body }}</p>
+       </div>
+
+       <div class="col-md-4">
+           <div class="well">
+               <dl class="dl-horizontal">
+                   <dt>Created At :</dt>
+                   <dd>{{ $post->created_at}}</dd>
+               </dl>
+               <dl class="dl-horizontal">
+                   <dt>Last Updated : </dt>
+                   <dd>{{ $post->updated_at }}</dd>
+               </dl>
+               <hr>
+               <div class="row">
+                   <div class="col-md-6">
+                       {!! Html::linkRoute('posts.edit','Edit',array($post->id), array('class'=>'btn btn-primary btn-block')) !!}
+
+                   </div>
+                   <div class="col-md-6">
+                       {!! Html::linkRoute('posts.destroy','Delete',array($post->id), array('class'=>'btn btn-danger btn-block')) !!}
+                   </div>
+               </div>
+           </div>
        </div>
 
    </div>
