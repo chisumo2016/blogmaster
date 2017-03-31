9493e3f7b80abf9b8e4ef8a2e43e51af085cdefd
Bernard Chisumo <bchisumo74@yahoo.com>
2017-03-31 16:41:53 +0100

    Implemented the query builder and display the information to the front page using pagesController

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -13,24 +13,15 @@
 
     <div class="row">
         <div class="col-md-8" >
+            @foreach($posts as $post)
             <div class="post">
-                <h3>Post Title</h3>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
+                <h2>{{ $post->title }}</h2>
+                <p class="lead"> {{ substr($post->body, 0 , 300)}} {{ strlen( $post->body) > 300 ? "......"  : ""}}  </p>
                 <a href="#" class="btn btn-primary">Read More</a>
             </div>
             <hr>
-            <div class="post">
-                <h3>Post Title</h3>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
-                <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-            <hr>
-            <div class="post">
-                <h3>Post Title</h3>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eius fugit ipsum pariatur ullam. Asperiores consectetur corporis cupiditate doloribus in labore ............</p>
-                <a href="#" class="btn btn-primary">Read More</a>
-            </div>
-            <hr>
+          @endforeach
+
         </div>
         <div class="col-md-3 col-md-offset-1">
             <h1>SideBar</h1>
